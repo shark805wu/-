66eb449a76d526891d0bff3cc1fe9637ddfa118b
shark805wu <shark805wu@gmail.com>
2020-12-27 13:15:33 +0800

    剩下一點點bug

--- FastfoodCrawler.py
+++ FastfoodCrawler.py
@@ -8,13 +8,13 @@ import csv
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-
+# '''
 calories_dic = {}
 with open('brand.csv', newline='') as csvfile:
     csv = csv.reader(csvfile, delimiter=',')
     count = 0
     for row in csv:
-        print(count)
+        print(row[0])
         time.sleep(3)
         if count == 12:
             break
@@ -36,12 +36,29 @@ with open('brand.csv', newline='') as csvfile:
 
         # 以下大類別不同（速食/冰...）要改
         for product_url in product_url_list:
-            product_url = 'https://foodtracer.taipei.gov.tw' + product_url
-            product_r = requests.get(product_url, verify=False)
-            product_soup = BeautifulSoup(product_r.text, 'html.parser')
-            product_name = product_soup.find('div', class_='crack').a.next_sibling.strip('\n \r /') # 品名
-            product_calories = product_soup.find('th', string='熱量(卡)').parent.next_sibling.next_sibling.contents[3].text
-            calories_dic[store_name][product_name] = float(product_calories)
+            try:
+                product_url = 'https://foodtracer.taipei.gov.tw' + product_url
+                product_r = requests.get(product_url, verify=False)
+                product_soup = BeautifulSoup(product_r.text, 'html.parser')
+                product_name = product_soup.find('div', class_='crack').a.next_sibling.strip('\n \r /') # 品名
+                product_calories = product_soup.find('th', string=re.compile('熱量')).parent.parent.contents[3].contents[3].text
+                calories_dic[store_name][product_name] = float(product_calories)
+            except:
+                print(product_name)
 
         count += 1
 print(calories_dic)
+# '''
+
+# '''
+# 測試區
+print('test')
+url = 'https://foodtracer.taipei.gov.tw/Front/Breakfast/ProductDetail?id=9509&pt=%e8%9b%8b%e9%a4%85'
+r = requests.get(url, verify=False)
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.find('div', class_='crack').a.next_sibling.strip('\n \r /'))
+# for i in range(10):
+print(soup.find('th', string='熱量(大卡)').parent.contents[2].text)
+print('test')
+# 測試區
+# '''
